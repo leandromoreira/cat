ad939083fa9eb9016661c10ff755bf85da387d0f
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2018-09-28 18:09:14 -0300

    add two nodes

--- scripts/provisioning.sh
+++ scripts/provisioning.sh
@@ -2,4 +2,4 @@ apt-get update -qq && apt-get install -y \
       strace nasm vim gcc sysstat man nginx python3 \
       sysstat ruby netcat-openbsd tcpdump traceroute mtr \
       iproute2 iputils-ping dnsutils net-tools lsof gdb \
-      ethtool wget curl time build-essential
+      ethtool wget curl time build-essential dstat
